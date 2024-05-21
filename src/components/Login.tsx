8dc52aa359b3ffd50ac1b3dae5db7d86f1d12a2b
Naxocist <73634957+Naxocist@users.noreply.github.com>
2024-05-21 16:55:43 +0700

    Arrange folder

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,7 +2,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './Login.css'
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { auth } from './firebase'
+import { auth } from '../ts/firebase'
 
 function Login() {
 
