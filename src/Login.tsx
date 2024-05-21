6377e4b89893fd79aa4c7c6fc2f88bc6cf80afc4
Naxocist <73634957+Naxocist@users.noreply.github.com>
2024-05-21 16:50:59 +0700

    Setup Firebase + Google login

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './Login.css'
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from './firebase'
 
-function App() {
-  const [count, setCount] = useState(0)
+function Login() {
+
+  const handleGoogle = () => {
+    const provider = new GoogleAuthProvider
+    return signInWithPopup(auth, provider)
+  }
 
   return (
     <>
@@ -16,21 +21,19 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Treatlie</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={ handleGoogle }>
+          Login with Google
         </button>
         <p>
-          This is edited file in another branch.
-          Another line to check if this is working.
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Enhances communication between doctors and patiences 
       </p>
     </>
   )
 }
 
-export default App
+export default Login
---
+++ src/firebase.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBHch1p8x_3_atUmPPGLnDBI092Rm5KkwE",
+  authDomain: "treatlie.firebaseapp.com",
+  projectId: "treatlie",
+  storageBucket: "treatlie.appspot.com",
+  messagingSenderId: "679615773376",
+  appId: "1:679615773376:web:15d6fd8386060b24654b25",
+  measurementId: "G-L89WEH5LEV",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export default app;
