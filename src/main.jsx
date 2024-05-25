cff2525f416d19bc7f58838c70d247a3e4db7879
Naxocist <73634957+Naxocist@users.noreply.github.com>
2024-05-25 19:49:11 +0700

    Fix ts error

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Login from './components/Login.tsx'
+import Login from './components/Login.jsx'
 import './css/index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Login />
   </React.StrictMode>,
