e9efd3d4fae20cdb46838c1787a7e8a6a59acc1b
Naxocist <subforwork@gmail.com>
2024-05-30 01:33:17 +0700

    remove prop-types

--- src/components/PatientCard.jsx
+++ src/components/PatientCard.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 
-function PatientCard({ name }) {
+function PatientCard({name}) {
 
-	return (
-		<div>
-			<h3>{name}</h3>
-		</div>
-	)
+  return (
+    <div>
+    <h3>{name}</h3>
+    </div>
+  )
 }
-
-PatientCard.propTypes = {}
 
 export default PatientCard
