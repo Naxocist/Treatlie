d09abe18591cdb22f80eaf62f90c8a45d97fc3d3
Naxocist <subforwork@gmail.com>
2024-06-03 02:39:04 +0700

    Add working input page and setup white theme

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,17 +1,59 @@
 import { useState } from 'react'
 
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone'
+
+import { Button, TextField } from '@mui/material';
+
+import { ref, set } from 'firebase/database';
+import { db, auth } from '../js/firebase'
+import { useNavigate } from 'react-router-dom';
+
+
 function Input() {
 
   const [name, setName] = useState('')
-  
+  const [date, setDate] = useState("")
+
+  dayjs.extend(utc)
+  dayjs.extend(timezone)
+
+  // const userTZ = dayjs.tz.guess()
+  const navigate = useNavigate()
+
+  const handleSubmit = () => {
+    const uid = auth.currentUser.uid
+    if(!uid) {
+      navigate('/')
+      return 
+    }
+
+    set(ref(db, '/info/' + uid), {
+      'name': name,
+      'birth-date': date,
+      'role': 'patient'
+    }).then(() => {
+      navigate('/patient')
+    })
+  }
+
   return (
     <div className='input-wrap'>
-      <div>
-        <input type='text'></input>
-      </div>
-      <div>
-        <input type='date'></input>
-      </div>
+      <TextField id="outlined-basic" label="Full Name" variant="outlined" onChange={(e) => setName(e.target.value)}/> 
+
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DatePicker onChange={(val) => {
+          setDate(dayjs(val).toISOString())
+        }} />
+      </LocalizationProvider>
+
+      <Button variant="contained" color="success" onClick={handleSubmit}>
+        Submit
+      </Button>
     </div>
   )
 }
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,16 +1,22 @@
+import dayjs from "dayjs"
 
 export function calculateAge(birthdayString) {
-  const [year, month, day] = birthdayString.split('-').map(Number)
+  const dateObj = dayjs(birthdayString)
+
+  // const [year, month, day] = [dateObj.year(), dateObj.month(), dateObj.day()]
 
   
-  const birthday = new Date(year, month - 1, day)
+  const birthday = new Date(birthdayString)
   
   const today = new Date()
+
+  console.log(today, birthday)
   
   let age = today.getFullYear() - birthday.getFullYear()
   
   const monthDifference = today.getMonth() - birthday.getMonth()
   const dayDifference = today.getDate() - birthday.getDate()
+  console.log(monthDifference, dayDifference)
   
   if (monthDifference < 0 || (monthDifference === 0 && dayDifference < 0)) {
     age--
